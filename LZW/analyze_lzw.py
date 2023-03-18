2c8f9df019416ba517b529082340b4b9e7b21dbb
martasumyk <116710765+martasumyk@users.noreply.github.com>
2023-03-18 18:16:19 +0200

    Update analyze_lzw.py

--- LZW/analyze_lzw.py
+++ LZW/analyze_lzw.py
@@ -2,13 +2,15 @@ def analyze_lzw():
   '''
   Function to measure time and build graphics of the LZW algorithm.
   '''
+  data = open_file('text.txt')
   encoded = []
   decoded = []
   text = []
-  number_of_ch = [i for i in range(100, 10000, 100)]
+  number_of_ch = [i for i in range(100, 10000, 300)]
   for k in number_of_ch:
-    string1 = str(''.join(random.choices(string.ascii_lowercase + string.digits, k = k)))
+    string1 = data[:k]
     text.append(string1)
+
   for data in text:
     #Initialize the object of class Lzw:
     text_to_code = Lzw(data)
@@ -26,7 +28,6 @@ def analyze_lzw():
     decoding = text_to_code.decode(encoding)
     end2 = time.time()
     time_decode = end2 - start2
-
     decoded.append(time_decode)
 
   #Visualization of the result:
@@ -47,5 +48,7 @@ def analyze_lzw():
   plt.ylabel("Number of symbols")
   plt.title("LZW decoding")
   plt.show()
+  return f'So we can see that time that LZW algorithm takes to decode and encode data almost linearly depends on the size of input data.'
 
 print(analyze_lzw())
+
